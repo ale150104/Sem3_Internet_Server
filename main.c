42b64315b8492b208a4f225f220ed1f977d4f43b
Alessio Palumbo <ap-235896@hs-weingarten.de>
2025-01-09 18:13:57 +0100

    Main mit Initialisierung

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
+#include <stdlib.h>
+#include "connectionhandler.h"
+#include "util.h"
+#include <stdio.h>
 
+int main(int argc, char **argv)
+{
 
-int main(int argc) {
-    //...
+	utilInit(argv[0]);
+	infoPrint("HTTP server, group LAyer");	//TODO: Add your group number!
 
+	//TODO: evaluate command line arguments
+	//TODO: perform initialization
+
+	//TODO: use port specified via command line
+	infoPrint("Übergebener Port %s", argv[1]);
+	in_port_t port = 80;
+	
+	if(argv[1] != NULL)
+	{
+		port = (in_port_t) atoi(argv[1]);
+	} 
+
+	infoPrint("Port-Wert: %d", port);
+
+	const int result = connectionHandler((in_port_t)port);
+
+	return result != -1 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
+
+
+
+
